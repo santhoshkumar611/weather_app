6e4fd4171e7c94dce39b43cd26dbb77fb2d88f88
santhosh <santhoshsanti329@gmail.com>
2024-07-10 22:57:46 +0530

    completed

---
+++ weather-forecast/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const  App = () => {
+  return (
+    <div className ='app'>
+      <Weather></Weather>
+    </div>
+  )
+}
+
+export default  App
